acb6df601f9ec0a38f68b3536c5627d50c6ea4f3
Pedro <pemicama12@gmail.com>
2024-04-11 00:41:18 +0100

    some insights on q4 and rearranging some scripts

---
+++ stackBenck/analytical/exe_query.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 file_name"
+    exit 1
+fi
+
+file_name=$1
+sudo -u postgres psql -d stack -f $file_name
