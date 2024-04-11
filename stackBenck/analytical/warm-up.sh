7c62e8234a82d6abc9b54b3b3d209b4ac41f04b2
Pedro <pemicama12@gmail.com>
2024-04-11 12:03:25 +0100

    Refactor warm-up script to use explain_query.sh instead of exe_query.sh

--- stackBenck/analytical/warm-up.sh
+++ stackBenck/analytical/warm-up.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-./exe_query.sh Q1.sql > test_results/q1.txt
-
-./exe_query.sh Q2.sql > test_results/q2.txt
-
-./exe_query.sh Q3.sql > test_results/q3.txt
-
-./exe_query.sh Q4.sql > test_results/q4.txt
+./explain_query.sh Q1.sql > test_results/q1.txt
+./explain_query.sh Q2.sql > test_results/q2.txt
+./explain_query.sh Q3.sql > test_results/q3.txt
+./explain_query.sh Q4.sql > test_results/q4.txt
