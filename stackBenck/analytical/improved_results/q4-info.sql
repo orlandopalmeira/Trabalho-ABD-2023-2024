51def0c29def95495daab08fea98a36e24eb1c4e
orlandopalmeira <orlando.palmeira3@gmail.com>
2024-04-11 12:59:07 +0000

    . idk

--- stackBenck/analytical/improved_results/q4-info.sql
+++ stackBenck/analytical/improved_results/q4-info.sql
@@ -1,11 +1,11 @@
 -- Active: 1708441413272@@127.0.0.1@5432@stack
-# Índices para a query 4
-# pensou-se em criar um indice para a data, mas verificamos que não é utilizado o indice, uma vez que o agrupamento é feito em relação ao resultado de date_bin e não da data em si (por verificar com EXPLAIN ANALYZE)
-CREATE INDEX idx_badges_date ON badges(date);
+-- Índices para a query 4
+-- pensou-se em criar um indice para a data, mas verificamos que não é utilizado o indice, uma vez que o agrupamento é feito em relação ao resultado de date_bin e não da data em si (por verificar com EXPLAIN ANALYZE)
+-- CREATE INDEX idx_badges_date ON badges(date);
 
-# Também não parece ser util este indice
-CREATE INDEX idx_badges_name_class_userid ON badges(tagbased, name, class, userid);
+-- Também não parece ser util este indice
+-- CREATE INDEX idx_badges_name_class_userid ON badges(tagbased, userid, class, name);
 
 
-DROP INDEX idx_badges_date;
+-- DROP INDEX idx_badges_date;
 DROP INDEX idx_badges_name_class_userid;
