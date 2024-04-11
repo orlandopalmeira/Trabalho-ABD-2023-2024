75689d7d6518cd6de7b1eba2ba156e2a95d67563
orlandopalmeira <orlando.palmeira3@gmail.com>
2024-04-11 11:00:43 +0000

    warmup.sh

---
+++ stackBenck/analytical/warm-up.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+./exe_query.sh Q1.sql > test_results/q1.txt
+
+./exe_query.sh Q2.sql > test_results/q2.txt
+
+./exe_query.sh Q3.sql > test_results/q3.txt
+
+./exe_query.sh Q4.sql > test_results/q4.txt
