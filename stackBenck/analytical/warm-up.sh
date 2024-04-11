dcbc3368ef001aff60e356573b97f92db7cdab8a
Pedro <pemicama12@gmail.com>
2024-04-11 12:03:57 +0100

    Refactor warm-up script to use lowercase file names in explain_query.sh calls

--- stackBenck/analytical/warm-up.sh
+++ stackBenck/analytical/warm-up.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./explain_query.sh Q1.sql > test_results/q1.txt
-./explain_query.sh Q2.sql > test_results/q2.txt
-./explain_query.sh Q3.sql > test_results/q3.txt
-./explain_query.sh Q4.sql > test_results/q4.txt
+./explain_query.sh q1.sql > test_results/q1.txt
+./explain_query.sh q2.sql > test_results/q2.txt
+./explain_query.sh q3.sql > test_results/q3.txt
+./explain_query.sh q4.sql > test_results/q4.txt
