2fb0466b26da1d51e8ef4892f7494139dafdda59
orlandopalmeira <orlando.palmeira3@gmail.com>
2024-04-12 11:56:27 +0000

    Reorganization

--- stackBenck/analytical/explain_query.sh
+++ stackBenck/analytical/explain_query.sh
@@ -8,7 +8,7 @@ fi
 
 file_name=$1
 # comando="EXPLAIN ANALYZE $(cat $file_name)"
-# comando="EXPLAIN (ANALYZE, BUFFERS) $(cat $file_name)"
-comando="EXPLAIN (ANALYZE, COSTS, VERBOSE, BUFFERS, FORMAT JSON) $(cat $file_name)"
+comando="EXPLAIN (ANALYZE, BUFFERS) $(cat $file_name)"
+# comando="EXPLAIN (ANALYZE, COSTS, VERBOSE, BUFFERS, FORMAT JSON) $(cat $file_name)"
 
 psql -d stack -c "$comando"
--- stackBenck/analytical/warm-up.sh
+++ stackBenck/analytical/warm-up.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-./explain_query.sh q1.sql > test_results/q1.txt
-./explain_query.sh q2.sql > test_results/q2.txt
-./explain_query.sh q3.sql > test_results/q3.txt
-./explain_query.sh q4.sql > test_results/q4.txt
+# ./explain_query.sh q1.sql > test_results/q1.txt
+# ./explain_query.sh q2.sql > test_results/q2.txt
+# ./explain_query.sh q3.sql > test_results/q3.txt
+# ./explain_query.sh q4.sql > test_results/q4.txt
+
+# comando="EXPLAIN (ANALYZE, BUFFERS) $(cat q1.sql)"
+
+psql -d stack -c "EXPLAIN (ANALYZE, BUFFERS) $(cat q1.sql)" > /dev/null
+psql -d stack -c "EXPLAIN (ANALYZE, BUFFERS) $(cat q2.sql)" > /dev/null
+psql -d stack -c "EXPLAIN (ANALYZE, BUFFERS) $(cat q3.sql)" > /dev/null
+psql -d stack -c "EXPLAIN (ANALYZE, BUFFERS) $(cat q4.sql)" > /dev/null
