4f4ea33bcc4c77da3b102e7b1659c2f6f4749050
Pedro <pemicama12@gmail.com>
2024-04-14 13:25:25 +0100

    reorganization of analytical query files

--- stackBenck/analytical/q4/q4-improvs.sql
+++ stackBenck/analytical/q4/q4-improvs.sql
@@ -1,5 +1,3 @@
--- Active: 1712845059097@@127.0.0.1@5432@stack
--- # Índices para a query 4
 -- # pensou-se em criar um indice para a data, mas verificamos que não é utilizado o indice, uma vez que o agrupamento é feito em relação ao resultado de date_bin e não da data em si (por verificar com EXPLAIN ANALYZE)
 -- # um indice nos nomes também não se provou muito útil, mas não sei bem porquê (talvez porque o filtro é feito em relação a uma lista de valores)(verificar melhor a veracidade disto)
 -- # um indice no campo tagbased também não se provou muito útil 
@@ -9,7 +7,7 @@
 CREATE INDEX idx_badges_date ON badges_mat_view (date);
 DROP INDEX IF EXISTS idx_badges_date;
 
--- Indices de hash(nem consegui criar o indice)
+-- Indices de hash demoram muito tempo a criar devido a colisoes, não tendo sido uma estratégia a seguir.
 CREATE INDEX idx_badges_name ON badges USING hash (name);
 
 
