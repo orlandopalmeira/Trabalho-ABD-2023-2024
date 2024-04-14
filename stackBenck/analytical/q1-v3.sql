9b5aa988296c99b6847e30aaf0a35cbee1c0de05
Pedro <pemicama12@gmail.com>
2024-04-14 12:36:16 +0100

    q1-v3 testing

---
+++ stackBenck/analytical/q1-v3.sql
@@ -0,0 +1,19 @@
+SELECT u.id, u.displayname, (COALESCE(qcount, 0)+COALESCE(acount, 0)+COALESCE(ccount, 0)) AS total
+FROM users u
+LEFT JOIN (
+    SELECT owneruserid, count(*) AS qcount
+    FROM questions WHERE creationdate BETWEEN now() - interval '6 months' AND now()
+    GROUP BY owneruserid
+) q ON q.owneruserid = u.id
+LEFT JOIN (
+    SELECT owneruserid, count(*) AS acount
+    FROM answers WHERE creationdate BETWEEN now() - interval '6 months' AND now()
+    GROUP BY owneruserid
+) a ON a.owneruserid = u.id
+LEFT JOIN (
+    SELECT userid, count(*) AS ccount
+    FROM comments WHERE creationdate BETWEEN now() - interval '6 months' AND now()
+    GROUP BY userid
+) c ON c.userid = u.id
+ORDER BY total DESC
+LIMIT 100;
