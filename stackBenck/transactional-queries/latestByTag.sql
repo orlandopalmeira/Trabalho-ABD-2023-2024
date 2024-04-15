29d2a1207d01b04ba02783473cfb20cfde4190a0
Miguelcj1 <miguelluffy7@gmail.com>
2024-04-15 22:28:35 +0100

    correção em um indice e modificação na query LatestByTag (mesma performance)

--- stackBenck/transactional-queries/latestByTag.sql
+++ stackBenck/transactional-queries/latestByTag.sql
@@ -3,13 +3,25 @@ from questions q
 join questionstags qt on qt.questionid = q.id
 where qt.tagid = 1
 order by q.creationdate desc
-limit 25
+limit 25;
+
+-- Modificação na query (remoção do join) (Mesma performance)
+WITH tagged_questions AS (
+    SELECT id, title, creationdate
+    FROM questions
+    WHERE id IN (SELECT questionid FROM questionstags WHERE tagid = 1)
+)
+SELECT id, title
+FROM tagged_questions tg
+ORDER BY tg.creationdate DESC
+LIMIT 25;
+
 
 CREATE INDEX idx_questionstags_tagid ON questionstags (tagid);
 CREATE INDEX idx_questions_id ON questions (id); -- Repetido no questionInfo.sql
-CREATE INDEX idx_questions_creationdate_desc ON questions (creationdate DESC);
+CREATE INDEX idx_questions_creationdate ON questions (creationdate);
 
 
 DROP INDEX IF EXISTS idx_questionstags_tagid;
 DROP INDEX IF EXISTS idx_questions_id;
-DROP INDEX IF EXISTS idx_questions_creationdate_desc;
+DROP INDEX IF EXISTS idx_questions_creationdate;
