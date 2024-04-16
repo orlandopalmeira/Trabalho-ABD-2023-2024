a58d27f6635819a9058138957c120a2e2f98ecb1
orlandopalmeira <orlando.palmeira3@gmail.com>
2024-04-16 14:51:13 +0000

    optimizations q3

--- stackBenck/analytical/q3/q3-v3.sql
+++ stackBenck/analytical/q3/q3-v3.sql
@@ -1,16 +1,16 @@
-SELECT tagname, ROUND(AVG(total), 3), COUNT(*)
+SELECT t.tagname, round(avg(total), 3), count(*)
 FROM (
-    SELECT t.tagname, qt.questionid, COUNT(a.parentid) AS total
-    FROM tags t
-    JOIN questionstags qt ON qt.tagid = t.id
+    SELECT qt.tagid, qt.questionid, count(*) AS total
+    FROM questionstags qt
     LEFT JOIN answers a ON a.parentid = qt.questionid
-    WHERE t.id IN (
+    WHERE qt.tagid IN (
         SELECT tagid
         FROM questionstags
         GROUP BY tagid
-        HAVING COUNT(*) > 10
+        HAVING count(*) > 10
     )
-    GROUP BY t.tagname, qt.questionid
-) AS subquery
-GROUP BY tagname
+    GROUP BY qt.tagid, qt.questionid
+)
+LEFT JOIN tags t ON t.id = tagid
+GROUP BY t.tagname
 ORDER BY 2 DESC, 3 DESC, tagname;
