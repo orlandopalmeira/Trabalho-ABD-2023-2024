069bfdfd174e957d8c4a50c9d8c85b542bdcdc99
orlandopalmeira <orlando.palmeira3@gmail.com>
2024-04-27 20:15:01 +0100

    analytic q2 limpeza

---
+++ stackBenck/analytical/q2/indexes.sql
@@ -0,0 +1,5 @@
+CREATE INDEX idx_votes_creationdate ON votes (creationdate);
+CREATE INDEX idx_users_creationdate ON users (creationdate);
+
+-- DROP INDEX idx_votes_creationdate;
+-- DROP INDEX idx_users_creationdate;
