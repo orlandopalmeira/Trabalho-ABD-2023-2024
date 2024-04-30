2d09ce6e29234c83b3d582510d6ee0f83c161c99
orlandopalmeira <orlando.palmeira3@gmail.com>
2024-04-30 15:47:09 +0100

    novo índice na q2

--- stackBenck/analytical/q2/indexes.sql
+++ stackBenck/analytical/q2/indexes.sql
@@ -1,5 +1,8 @@
 CREATE INDEX idx_votes_creationdate ON votes (creationdate);
 CREATE INDEX idx_users_creationdate ON users (creationdate);
 
+CREATE INDEX idx_users_creationdate_year on users (extract(year from creationdate));
+
 -- DROP INDEX idx_votes_creationdate;
 -- DROP INDEX idx_users_creationdate;
+-- DROP INDEX idx_users_creationdate_year
