e915aaa872e734aa0b8ef60a27085066e9e748a2
Pedro <pemicama12@gmail.com>
2024-05-27 22:55:39 +0100

    Refactor script to use final versions of queries

--- stackBenck/analytical/finals.sh
+++ stackBenck/analytical/finals.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+echo "Query 1"
 python3 arg_script.py q1/q1-vfinal.sql
+echo "Query 2"
 python3 arg_script.py q2/q2-final.sql
+echo "Query 3"
 python3 arg_script.py q3/q3-v2.sql
+echo "Query 4"
 python3 arg_script.py q4/q4-v2.sql
